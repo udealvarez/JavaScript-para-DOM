d5d7d990adc2cb195000c8497302c6d85acde076
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-18 15:08:10 -0300

    Seleccionamos elementos y le damos estilos desde el script

---
+++ 01-JavaScript-en-la-web/3-Manipulacion-de-DOM/5-Agregar-quitar-estilos/script.js
@@ -0,0 +1,9 @@
+//  const color = document.getElementById("styles").style.color 
+//  const background = document.getElementById("styles").style.backgroundColor
+
+
+const container = document.getElementById('div')
+container.querySelector('p.style').innerHTML = 'Texto desde JS'
+container.querySelector('p.style').style.color = 'red'
+
+
