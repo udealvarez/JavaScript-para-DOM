13267877ddbf5e5df98867ff8ec45deb2996c8a5
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-19 12:39:44 -0300

    API History | ir hacia adelante o atras en la url

---
+++ 02-APIs/5-History-API/script.js
@@ -0,0 +1,7 @@
+
+
+const goBackx2 = () => window.history.go(-2)        // nos permite ir 2 veces hacia atras
+const goBack = () => window.history.back()          // nos permite ir hacia atras una sola vez
+const goForward = () => window.history.forward()    // nos permite ir hacia adelante una sola vez
+const goForwardX2 = () => window.history.go(2)      // nos permite ir 2 veces hacia adelante
+
